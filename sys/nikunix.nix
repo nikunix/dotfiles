2408d1ac1726e7c0970374ba9b55c8f9546c1aea
nikunix <nikunix@proton.me>
2025-06-12 21:42:45 +0200

    Forgot networkManager.

--- sys/nikunix.nix
+++ sys/nikunix.nix
@@ -1,7 +1,10 @@
 {...}: # The one on my laptop.
 
 {
-  networking.hostName = "nikUnix";
+  networking = {
+    hostName = "nikUnix";
+    networkmanager.enable = true;
+  };
 
   imports = [
     ../soft/softNikunix.nix
