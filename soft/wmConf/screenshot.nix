1561564c20cc04102675e90b56e62ebfd214d025
nikunix <nikunix@proton.me>
2025-06-12 20:59:51 +0200

    Some stuff...

--- soft/wmConf/screenshot.nix
+++ soft/wmConf/screenshot.nix
@@ -12,7 +12,7 @@ case $@ in
     swaymsg -t get_tree | ${pkgs.jq}/bin/jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"' | ${pkgs.grim}/bin/grim -g - - | ${pkgs.wl-clipboard}/bin/wl-copy
     ;;
 esac
-if [ $(${pkgs.libnotif}/bin/notify-send --action 'default=default' "Edit or save screenshot?") == default ]; then
+if [ $(${pkgs.libnotify}/bin/notify-send --action 'default=default' "Edit or save screenshot?") == default ]; then
   ${pkgs.wl-clipboard}/bin/wl-paste | swappy -f -
 fi
 ''
